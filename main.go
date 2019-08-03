a8445c5b2bb08907a2359f725ca7ffddfc62e372
willerhe <willerhe@126.com>
2019-08-03 10:56:21 +0800

    remove router

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"code.qlteam.com/model"
-	"code.qlteam.com/router"
+	"code.qlteam.com/rest"
 	"github.com/willerhe/webbase/apper"
+	"github.com/willerhe/webbase/router"
 )
 
 func main() {
 	app := apper.New()
 
+	defer app.Start()
+
 	// 迁移模型
 	model.Sync()
-	router.Mount(&app.RouterGroup)
 
-	defer app.Start()
+	// 挂在路由
+	api := app.RouterGroup.Group("api")
+
+	router.Register(api, new(rest.Project)) // 项目api
 
 }
