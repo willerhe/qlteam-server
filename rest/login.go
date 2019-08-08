2febfc84cea3744c80897a1437db42f3d548d208
hzw2019 <hewei110n>
2019-08-08 15:29:59 +0800

    fix bug

---
+++ rest/login.go
@@ -0,0 +1,11 @@
+package rest
+
+import "github.com/gin-gonic/gin"
+
+type Login int
+
+func (l *Login) Register(router *gin.RouterGroup) {
+	//r := router.Group("")
+	//r.GET("/projects", p.list)
+	//r.GET("/project/:id", p.get)
+}
--- rest/story_test.go
+++ rest/story_test.go
@@ -11,10 +11,10 @@ func TestStory(t *testing.T) {
 	s := &model.Story{
 		Describe: "测试使用的用户故事",
 	}
-	service.Story.Save(s)
+	service.Story.Create(s)
 	fmt.Println("创建后的id是", s.ID)
 	s.Describe = ""
-	service.Story.Save(s)
+	service.Story.Update(s)
 	fmt.Println("修改用户故事", s)
 	service.Story.Delete(s)
 	fmt.Println("删除用户故事，id 为", s.ID)
