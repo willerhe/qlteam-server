a17269b9e9af8fc939fa671d5cac5f2d769721ca
willerhe <willerhe@126.com>
2019-08-10 07:36:39 +0800

    add api verson

--- main.go
+++ main.go
@@ -15,17 +15,18 @@ func main() {
 	model.Sync()
 	// 跟路由
 	root := &app.RouterGroup
-	// api 子路由
 	api := root.Group("api")
+	// api 子路由
+	v1 := api.Group("v1")
 
 	// 登录
 	router.Register(root, new(rest.Login))
 
-	api.Use(middleware.AllowCORS, middleware.MustLogged)
+	v1.Use(middleware.AllowCORS, middleware.MustLogged)
 
-	router.Register(api, new(rest.Project)) // 项目api
-	router.Register(api, new(rest.Story))   // 用户故事api
-	router.Register(api, new(rest.Task))    // 任务
-	router.Register(api, new(rest.Login))
+	router.Register(v1, new(rest.Project)) // 项目api
+	router.Register(v1, new(rest.Story))   // 用户故事api
+	router.Register(v1, new(rest.Task))    // 任务
+	router.Register(v1, new(rest.Login))
 
 }
