dca5bce835ca6576ec8cacbc5201b83f0a72a1af
willerhe <willerhe@126.com>
2019-08-14 07:31:30 +0800

    add token validate

--- service/token.go
+++ service/token.go
@@ -4,15 +4,21 @@ import (
 	"code.qlteam.com/model"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/willerhe/webbase/configer"
+	"time"
 )
 
 type token int
 
 var Token token
 
+// todo TOKEN2 生成token
 func (token) General(user model.User) string {
 	sign := []byte(configer.Config.Get("token.sign").(string))
-	claims := &jwt.StandardClaims{}
+	claims := new(model.Token)
+	// 过期时间 1小时
+	claims.ExpiresAt = time.Now().Add(time.Hour * time.Duration(1)).Unix()
+	claims.IssuedAt = time.Now().Unix()
+	claims.UID = user.ID
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	ss, _ := token.SignedString(sign)
 	return ss
