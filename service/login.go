258583faca0ce47043857d1a978105a647165436
willerhe <willerhe@126.com>
2019-08-14 07:09:23 +0800

    add register

--- service/login.go
+++ service/login.go
@@ -3,6 +3,8 @@ package service
 import (
 	"code.qlteam.com/errset"
 	"code.qlteam.com/model"
+	md52 "crypto/md5"
+	"fmt"
 	"github.com/willerhe/webbase/orm"
 )
 
@@ -13,11 +15,14 @@ var Login login
 // Register 注册用户
 func (login) Register(u *model.User) error {
 	// 1. 判断是否存在
-
 	count := orm.DB.SqlSession.Where("account = ?", u.Account).First(u).RowsAffected
 	if count > 0 {
 		return errset.AccountExisted
 	}
+	// md5
+	hash := md52.New()
+	hash.Write([]byte(u.Password))
+	u.Password = fmt.Sprintf("%X", hash.Sum(nil))
 	// 2. 新建用户
 	orm.DB.SqlSession.NewRecord(u)
 	orm.DB.SqlSession.Omit("deleted_at", "updated_at").Create(u)
